dd0c27f5ec6c8ba9888fc6322a44d426f6238084
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2024-08-05 16:59:11 +0300

    react router + base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+import "./index.scss";
 
-function App() {
-  return <>Hello World!</>;
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "*",
+    element: <div>404 Not Found</div>,
+  },
+]);
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <RouterProvider router={router} />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
